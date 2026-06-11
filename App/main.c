7e5998f5c73071cc76c6e2d3515c03f2a1d76e75
larry <larryyiukg@gmail.com>
2026-06-11 16:44:50 +0800

    chore: sht20 add state machine approach

--- App/main.c
+++ App/main.c
@@ -8,20 +8,20 @@
 
 void ShortPressListener(void)
 {
-    // float temp     = SHT20_GetTemp(true);
-    // float humidity = SHT20_GetHumidity(true);
-    // if(!isnan(temp))
-    // {
-    //     int32_t temp = (int32_t)(SHT20_GetTemp(true) * 100.0f);
-    //     DBG_log("[SHT20] Temp: %d.%02d C\n", temp / 100, ((temp % 100) > 0 ? abs(temp % 100) : 0));
-    // }
+    float _temp     = SHT20_GetTemp(false);
+    float _humidity = SHT20_GetHumidity(false);
+    if(!isnan(_temp))
+    {
+        int32_t temp = (int32_t)(_temp * 100.0f);
+        DBG_log("[SHT20] Temp: %d.%02d C\n", temp / 100, ((temp % 100) > 0 ? abs(temp % 100) : 0));
+    }
 
-    // if(!isnan(humidity))
-    // {
-    //     int32_t humidity = (int32_t)(SHT20_GetHumidity(true) * 100.0f);
-    //     DBG_log("[SHT20] Humidity: %d.%02d %%\n", humidity / 100, humidity % 100);
-    // }
-    SHT20_test();
+    if(!isnan(_humidity))
+    {
+        uint32_t humidity = (uint32_t)(_humidity * 100.0f);
+        DBG_log("[SHT20] Humidity: %d.%02d %%\n", humidity / 100, humidity % 100);
+    }
+    // SHT20_test();
 }
 
 int main()
@@ -37,6 +37,6 @@ int main()
     while(1)
     {
         KEY_Scan(0);
-        // SHT20_Run();
+        SHT20_Run();
     }
 }
--- Driver/sht20_driver.c
+++ Driver/sht20_driver.c
@@ -5,6 +5,7 @@
 #include "gpio_decoder.h"
 #include "dwt_delay.h"
 #include "RTT_Debug.h"
+#include "systick.h"
 
 #define __SCL_PORT GPIO_GetPeriphAddr(SHT20_SCL_PIN)
 #define __SCL_PIN GPIO_GetPinAddr(SHT20_SCL_PIN)
@@ -31,6 +32,30 @@
 // #define __InterpretTemp(msb, lsb) ((float)(((((uint16_t)msb) << 8) | lsb) >> 2) * 175.72f / 65536.0f - 46.85f)
 // #define __InterpretHumidity(msb, lsb) ((float)(((((uint16_t)msb) << 8) | lsb) >> 2) * 125.0f / 65536.0f - 6.0f)
 
+typedef enum __SHT20_STATE __SHT20_STATE_t;
+enum __SHT20_STATE
+{
+    __STATE_IDLE,
+    __STATE_CMD,
+    __STATE_WAIT,
+    __STATE_READ,
+    __STATE_CRC,
+    __STATE_INTERPRET
+};
+
+static __SHT20_STATE_t __state = __STATE_CMD;
+
+static bool __isMeasuringTemp = true;
+
+static float __lastTemp     = 0.0f;
+static float __lastHumidity = 0.0f;
+
+static uint64_t __lastTempUpdatedAt     = 0;
+static uint64_t __lastHumidityUpdatedAt = 0;
+
+static bool __tempUpdated     = false;
+static bool __humidityUpdated = false;
+
 void __GPIO_Config()
 {
     /* SCL pin, 0.4 MHz at max */
@@ -45,9 +70,30 @@ void __GPIO_Config()
 void SHT20_Init(void)
 {
     __GPIO_Config();
+    __lastTemp     = NAN;
+    __lastHumidity = NAN;
 }
 
-void __Start(void)
+void SHT20_Enable(void)
+{
+    rcu_periph_clock_enable(GPIO_GetRcuPeriph(SHT20_SCL_PIN));
+    gpio_init(__SCL_PORT, GPIO_MODE_OUT_OD, GPIO_OSPEED_50MHZ, __SCL_PIN);
+    rcu_periph_clock_enable(GPIO_GetRcuPeriph(SHT20_SDA_PIN));
+    gpio_init(__SDA_PORT, GPIO_MODE_OUT_OD, GPIO_OSPEED_50MHZ, __SDA_PIN);
+    __state           = __STATE_CMD;
+    __isMeasuringTemp = true;
+}
+
+void SHT20_Disable(void)
+{
+    rcu_periph_clock_disable(GPIO_GetRcuPeriph(SHT20_SCL_PIN));
+    rcu_periph_clock_disable(GPIO_GetRcuPeriph(SHT20_SDA_PIN));
+    gpio_init(__SCL_PORT, GPIO_MODE_AIN, GPIO_OSPEED_50MHZ, __SCL_PIN);
+    gpio_init(__SDA_PORT, GPIO_MODE_AIN, GPIO_OSPEED_50MHZ, __SDA_PIN);
+    __state = __STATE_IDLE;
+}
+
+void __forceinline __Start(void)
 {
     __SDA_High();
     __SCL_High();
@@ -57,7 +103,7 @@ void __Start(void)
     __SCL_Low();
 }
 
-void __Stop(void)
+void __forceinline __Stop(void)
 {
     __SDA_Low();
     DWT_Delay_us(10);
@@ -67,7 +113,7 @@ void __Stop(void)
     DWT_Delay_us(20);
 }
 
-void __SendAck(bool isAck)
+void __forceinline __SendAck(bool isAck)
 {
     if(isAck)
     {
@@ -84,7 +130,7 @@ void __SendAck(bool isAck)
     DWT_Delay_us(20);
 }
 
-bool __RecvAck()
+bool __forceinline __RecvAck()
 {
     __SDA_High(); // Release SDA for ACK
     DWT_Delay_us(10);
@@ -191,21 +237,22 @@ float SHT20_test(void)
         DBG_log("Write cmd start write failed.\n");
         return NAN;
     }
-    if(!__SHT20_WriteByte(__CMD_MEASURE_HUMIDITY_HOLD))
+    if(!__SHT20_WriteByte(__CMD_MEASURE_HUMIDITY_NO_HOLD))
     {
         DBG_log("Write cmd read temp failed.\n");
         return NAN;
     }
     DWT_Delay_us(20);
     __Start();
-    if(!__SHT20_WriteByte(__CMD_START_READ))
+    while(!__SHT20_WriteByte(__CMD_START_READ))
     {
-        DBG_log("Write cmd start read failed.\n");
-        return NAN;
+        DBG_log("data not ready.\n");
+        __Stop();
+        DWT_Delay_us(20);
+        __Start();
     }
-    // busy wait until the sensor is ready
-
-    DWT_Delay_ms(100);
+    //__SCL_High();
+    // while(gpio_input_bit_get(__SCL_PORT, __SCL_PIN) == RESET);
     uint8_t temp_msb = __SHT20_ReadByte(false);
     uint8_t temp_lsb = __SHT20_ReadByte(false);
     uint8_t temp_crc = __SHT20_ReadByte(true);
@@ -224,4 +271,133 @@ float SHT20_test(void)
     DBG_log("lsb: 0x%08x\n", temp_lsb);
     DBG_log("temp: %d.%02d C\n", temp / 100, temp % 100);
     return temp / 100.0f;
+}
+
+void SHT20_Run(void)
+{
+    static uint8_t msb, lsb, crc;
+    switch(__state)
+    {
+        case __STATE_IDLE:
+            break;
+        case __STATE_CMD:
+            __SCL_High();
+            __Start();
+            if(!__SHT20_WriteByte(__CMD_START_WRITE))
+            {
+                DBG_log("Write cmd start write failed.\n");
+                return;
+            }
+            if(!__SHT20_WriteByte(__isMeasuringTemp ? __CMD_MEASURE_TEMPERATURE_NO_HOLD
+                                                    : __CMD_MEASURE_HUMIDITY_NO_HOLD))
+            {
+                DBG_log("Write cmd read %s failed.\n", __isMeasuringTemp ? "temp" : "humidity");
+                return;
+            }
+            DWT_Delay_us(20);
+            __state = __STATE_WAIT;
+            break;
+        case __STATE_WAIT:
+            __Start();
+            if(!__SHT20_WriteByte(__CMD_START_READ))
+            {
+                __Stop();
+                DWT_Delay_us(20);
+            }
+            else
+            {
+                __state = __STATE_READ;
+            }
+            break;
+        case __STATE_READ:
+            msb = __SHT20_ReadByte(false);
+            lsb = __SHT20_ReadByte(false);
+            crc = __SHT20_ReadByte(true);
+            __Stop();
+            __state = __STATE_CRC;
+            break;
+        case __STATE_CRC:
+            if(__CRC_Check(msb, lsb, crc))
+            {
+                // DBG_log("[SHT20 raw] msb: 0x%08x, lsb: 0x%08x, crc: 0x%08x\n", msb, lsb, crc);
+                __state = __STATE_INTERPRET;
+            }
+            else
+            {
+                DBG_log("[SHT20 Error] CRC check failed.\n");
+                __isMeasuringTemp = !__isMeasuringTemp;
+                __state           = __STATE_CMD;
+            }
+            break;
+        case __STATE_INTERPRET:
+            if(__isMeasuringTemp)
+            {
+                __lastTemp          = __InterpretTemp(msb, lsb);
+                __lastTempUpdatedAt = SYSTICK_GetSysRunTime();
+                __tempUpdated       = true;
+            }
+            else
+            {
+                __lastHumidity          = __InterpretHumidity(msb, lsb);
+                __lastHumidityUpdatedAt = SYSTICK_GetSysRunTime();
+                __humidityUpdated       = true;
+            }
+            __isMeasuringTemp = !__isMeasuringTemp;
+            msb = lsb = 0;
+            crc       = 0xff;
+            __state   = __STATE_CMD;
+            break;
+        default:
+            break;
+    }
+}
+
+float SHT20_GetTemp(bool handleError)
+{
+    if(!handleError)
+    {
+        return __lastTemp;
+    }
+    else
+    {
+        if(SYSTICK_GetSysRunTime() - __lastTempUpdatedAt > SHT20_ERROR_TIMEOUT_MS)
+        {
+            DBG_log("[SHT20 Error] Temperature reading timeout.\n");
+            return NAN;
+        }
+        else if(!__tempUpdated)
+        {
+            return NAN;
+        }
+        else
+        {
+            __tempUpdated = false;
+            return __lastTemp;
+        }
+    }
+}
+
+float SHT20_GetHumidity(bool handleError)
+{
+    if(!handleError)
+    {
+        return __lastHumidity;
+    }
+    else
+    {
+        if(SYSTICK_GetSysRunTime() - __lastHumidityUpdatedAt > SHT20_ERROR_TIMEOUT_MS)
+        {
+            DBG_log("[SHT20 Error] Humidity reading timeout.\n");
+            return NAN;
+        }
+        else if(!__humidityUpdated)
+        {
+            return NAN;
+        }
+        else
+        {
+            __humidityUpdated = false;
+            return __lastHumidity;
+        }
+    }
 }
