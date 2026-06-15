923e5526088b50378477cf6f306902b37dc50462
larry <larryyiukg@gmail.com>
2026-06-15 17:41:02 +0800

    chore: add avg filtering and histeresis for battery voltage

--- Driver/battery_lvl.c
+++ Driver/battery_lvl.c
@@ -4,6 +4,33 @@
 #include "dwt_delay.h"
 #include "RTT_Debug.h"
 
+#define __BAT_CHARGING_VOL_COMPENSATION \
+    (0.13f) // charging voltage is about 0.13V higher than actual voltage, therefore take away 0.13V to get more
+            // accurate voltage
+#define __BAT_ADC_RESOLUTION (4096.0f)
+#define __BAT_VOLTAGE_DIVIDER_RATIO (3.0f) // voltage divider 1:3, therefore the ratio is 4
+#define __BAT_VOLTAGE_DIVIDER_TOTAL (4.0f)
+#define __BAT_ADC_REF_VOLTAGE (3.3f)
+
+#define __BATTERY_VOLTAGE_MAX (4.2f)
+// #define __BATTERY_LEVEL_0 (__BATTERY_VOLTAGE_MAX * 0.2f) // 3.45V
+// #define __BATTERY_LEVEL_1 (__BATTERY_VOLTAGE_MAX * 0.4f) // [3.60V, 3.75V)
+// #define __BATTERY_LEVEL_2 (__BATTERY_VOLTAGE_MAX * 0.6f) // [3.75V, 3.90V)
+// #define __BATTERY_LEVEL_3 (__BATTERY_VOLTAGE_MAX * 0.8f) // 3.90V
+
+static const float __BATTERY_LEVEL_THRESHOLDS[] = {
+    3.45f, // 3.45V
+    3.60f, // [3.60V, 3.75V)
+    3.75f, // [3.75V, 3.90V)
+    3.90f, // 3.90V
+};
+
+#define __BAT_ADC_AVG_FILTER_SIZE (10)
+
+// histeresis filter to stabilize the voltage reading
+// https://www.microchip.com/en-us/about/media-center/blog/2021/implementing-hysteresis-in-the-adcc
+#define __BAT_HYSTERESIS_THRESHOLD (0.03f) // 30mV
+
 void __BAT_GPIO_Config()
 {
     // PC5 - ADC1_CH15
@@ -62,22 +89,86 @@ bool __IsUsbPlugged(void)
 
 float BAT_GetVoltage(void)
 {
-    adc_software_trigger_enable(ADC1, ADC_REGULAR_CHANNEL);
-    float adcVal;
-    while(SET != adc_flag_get(ADC1, ADC_FLAG_EOC));
-    adcVal = (float)adc_regular_data_read(ADC1);
-    adcVal += adcVal / 3; // voltage divider 1:3
+    float adcVal = 0.0f;
+    for(uint8_t i = 0; i < __BAT_ADC_AVG_FILTER_SIZE; i++)
+    {
+        adc_software_trigger_enable(ADC1, ADC_REGULAR_CHANNEL);
+        while(SET != adc_flag_get(ADC1, ADC_FLAG_EOC));
+        adcVal += adc_regular_data_read(ADC1);
+    }
+    adcVal /= __BAT_ADC_AVG_FILTER_SIZE;
+
+    adcVal = adcVal * __BAT_VOLTAGE_DIVIDER_TOTAL / __BAT_VOLTAGE_DIVIDER_RATIO; // voltage divider 1:3
+    adcVal = adcVal * __BAT_ADC_REF_VOLTAGE / __BAT_ADC_RESOLUTION;              // convert adc value to voltage
     if(__IsUsbPlugged())
     {
         DBG_log("Battery is charging\n");
-        adcVal -= 0.13f; // charging voltage is about 0.13V higher than actual voltage, therefore take away 0.13V to get
-                         // more accurate voltage
+        adcVal -= __BAT_CHARGING_VOL_COMPENSATION; // charging voltage is about 0.13V higher than actual voltage,
+                                                   // therefore take away 0.13V to get more accurate voltage
     }
-    return adcVal * 3.3f / 4096.0f;
+    return adcVal;
+}
+
+BAT_LVL_t BAT_GetLevel(float voltage)
+{
+    static BAT_LVL_t currLevel = BATTERY_LEVEL_NOT_INIT;
+    if(currLevel == BATTERY_LEVEL_NOT_INIT)
+    {
+        if(voltage < __BATTERY_LEVEL_THRESHOLDS[BATTERY_LEVEL_0])
+        {
+            currLevel = BATTERY_LEVEL_0;
+        }
+        else if(voltage < __BATTERY_LEVEL_THRESHOLDS[BATTERY_LEVEL_1])
+        {
+            currLevel = BATTERY_LEVEL_1;
+        }
+        else if(voltage < __BATTERY_LEVEL_THRESHOLDS[BATTERY_LEVEL_2])
+        {
+            currLevel = BATTERY_LEVEL_2;
+        }
+        else if(voltage < __BATTERY_LEVEL_THRESHOLDS[BATTERY_LEVEL_3])
+        {
+            currLevel = BATTERY_LEVEL_3;
+        }
+        else
+        {
+            currLevel = BATTERY_LEVEL_4;
+        }
+        return currLevel;
+    }
+
+    switch(currLevel)
+    {
+        case(BATTERY_LEVEL_0):
+            if(voltage > __BATTERY_LEVEL_THRESHOLDS[BATTERY_LEVEL_0] + __BAT_HYSTERESIS_THRESHOLD)
+            {
+                currLevel = BATTERY_LEVEL_1;
+            }
+            break;
+        case(BATTERY_LEVEL_4):
+            if(voltage < __BATTERY_LEVEL_THRESHOLDS[BATTERY_LEVEL_3] - __BAT_HYSTERESIS_THRESHOLD)
+            {
+                currLevel = BATTERY_LEVEL_3;
+            }
+            break;
+        default:
+            if(voltage < __BATTERY_LEVEL_THRESHOLDS[currLevel - 1] - __BAT_HYSTERESIS_THRESHOLD)
+            {
+                currLevel--;
+            }
+            else if(voltage > __BATTERY_LEVEL_THRESHOLDS[currLevel] + __BAT_HYSTERESIS_THRESHOLD)
+            {
+                currLevel++;
+            }
+            break;
+    }
+    return currLevel;
 }
 
 void BAT_Test(void)
 {
-    uint32_t adcVal = (uint32_t)(BAT_GetVoltage() * 100.0f);
-    DBG_log("Battery Voltage: %01d.%02d V\n", adcVal / 100, adcVal % 100);
+    float voltage   = BAT_GetVoltage();
+    BAT_LVL_t level = BAT_GetLevel(voltage);
+    uint32_t adcVal = (uint32_t)(voltage * 100.0f);
+    DBG_log("Battery Voltage: %01d.%02d V, Level: %d\n", adcVal / 100, adcVal % 100, (uint8_t)level);
 }
--- Driver/battery_lvl.h
+++ Driver/battery_lvl.h
@@ -3,11 +3,21 @@
 
 #include <stdint.h>
 
+typedef enum
+{
+    BATTERY_LEVEL_0 = 0,
+    BATTERY_LEVEL_1,
+    BATTERY_LEVEL_2,
+    BATTERY_LEVEL_3,
+    BATTERY_LEVEL_4,
+    BATTERY_LEVEL_NOT_INIT,
+} BAT_LVL_t;
+
 void BAT_Init(void);
 
 float BAT_GetVoltage(void);
 
-uint8_t BAT_GetLevel(void);
+BAT_LVL_t BAT_GetLevel(float voltage);
 
 void BAT_Test(void);
 
